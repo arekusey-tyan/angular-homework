9513f81cec0199b7cd30e12d586b794355fbacdb
Arekusey-tyan <43652041+arekusey-tyan@users.noreply.github.com>
2023-02-11 12:51:51 +0300

    New component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang1';
+  nowdate: Date = new Date();
+  books: Array<any> = [
+    {author:'Дойл, Артур Конан', booktitle:'Приключения Шерлока Холмса'},
+    {author:'Дойл, Артур Конан', booktitle:'Собака Баскервилей'},
+    {author:'Дюма, Александр', booktitle:'Граф Монте-Кристо'},
+    {author:'Хейли, Артур', booktitle:'Аэропорт'}
+  ];
 }
